b4f1a10ae0e2127703fd87900ced55d2700ac9bf
Anson Ho <48861078+spectroscopycafe@users.noreply.github.com>
2021-12-25 20:09:08 -0500

    update DQN file with latest attempt

--- algorithms/DQN.py
+++ algorithms/DQN.py
@@ -8,21 +8,19 @@ Anson Ho, 2021
 import gym
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import random
-import numpy as np
-from collections import deque
 import wandb
+import numpy as np
 
 from assets.memorybuffer import MemoryBuffer
 
 
-WANDB = 0
+WANDB = 1
 
 if WANDB:
     wandb.init(project="DQN", entity="ansonwhho")
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def make_environment(game, num_frame_stack):
     """
@@ -50,36 +48,46 @@ def make_environment(game, num_frame_stack):
 class DQN(nn.Module):
     """
     DQN for approximating the Q-function
-    The input is a 4 x 84 x 84 image
+    The input is a 84 x 84 image
+    Input channels = 4 to account for the stacked frames
     """
+
+    # Note on fc1: see https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+    # 32 channels x 9 width x 9 height
+    # input has width 84 and height 84
+    # conv1 outputs (84 - 8)/4 + 1 = 20
+    # conv2 outputs (20 - 4)/2 + 1 = 9
+    # so we expect 9 width and 9 height
 
     def __init__(self, num_actions):
         super(DQN, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv3d(in_channels=1, out_channels=16, kernel_size=(4,8,8), stride=4),
-            nn.ReLU()
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv3d(in_channels=16, out_channels=32, kernel_size=(1,4,4), stride=2),
-            nn.ReLU()            
-        )
-        self.fc1 = nn.Sequential(
-            torch.nn.Linear(2592, 256), # https://pytorch.org/docs/stable/generated/torch.nn.Conv3d.html
-            nn.ReLU()
-        )
-        self.fc2 = nn.Sequential(
-            torch.nn.Linear(256, num_actions),
-            nn.ReLU()
-        )
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2)
+        self.fc1 = torch.nn.Linear(32 * 9 * 9, 256)
+        self.head = torch.nn.Linear(256, num_actions)
         self.loss_function = nn.MSELoss()
     
     def forward_pass(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = torch.flatten(x)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.fc1(x.view(x.size(0), -1)))
+        x = self.head(x)
         return x
+
+def get_state(observation):
+    """
+    Observation is of form 
+    channels x width x height x batch_size
+    want the state to be of form
+    channels x height x width
+    """
+
+    state = torch.from_numpy(observation.__array__())
+    # state = torch.from_numpy(observation.__array__()) / 255 # will this cause vanishing gradients?
+    state = state.permute((3,0,1,2))
+    # print(state.shape) [1,4,84,84] after permuting
+
+    return state
 
 def train_model(minibatch, discount):
     """
@@ -87,7 +95,7 @@ def train_model(minibatch, discount):
     Applies to both the policy and target nets
 
     Takes a minibatch where each element is of form:
-    [phi_j, a_j, r_j, phi_j+1]
+    [state, action, reward, next_state]
 
     """  
 
@@ -96,22 +104,33 @@ def train_model(minibatch, discount):
     reward_minibatch = torch.tensor(reward_minibatch)
     next_state_minibatch = [transition[3] for transition in minibatch]
     done_minibatch= [transition[4] for transition in minibatch]
-    done_minibatch = torch.Tensor(done_minibatch)
+    done_minibatch = torch.tensor(done_minibatch)
 
-    target_q_values = [target_net.forward_pass(state) for state in next_state_minibatch]
+
+    target_q_values = [target_net.forward_pass(state)[0] for state in next_state_minibatch]
     target_q_values = torch.stack(target_q_values, dim=0)
-    targets = torch.add(reward_minibatch,  (1 - done_minibatch) * discount * torch.max(target_q_values, dim=1)[0])
 
-    policy_values = [policy_net.forward_pass(state) for state in state_minibatch]
+    targets = torch.add(reward_minibatch,  (1 - done_minibatch.long()) * discount * torch.max(target_q_values, dim=1)[0])
+
+    policy_values = [policy_net.forward_pass(state)[0] for state in state_minibatch]
     policy_values = torch.stack(policy_values, dim=0)
     predictions = torch.max(policy_values, dim=1)[0]
 
-    # target = reward + discount * max(target_net.forward_pass(next_state))
+    # target = torch.add(reward, discount * max(target_net.forward_pass(next_state)))
     # prediction = max(policy_net.forward_pass(state))
-    optimizer = torch.optim.RMSprop(policy_net.parameters(), lr=learning_rate)
-
+    
     loss = policy_net.loss_function(predictions, targets)
     # loss = policy_net.loss_function(prediction, target)
+
+    if WANDB:
+        wandb.log({
+            "loss": loss,
+            "epsilon": epsilon,
+            "avg_q_value": avg_q_value,
+            "avg_reward": avg_reward
+            })
+
+    optimizer = torch.optim.RMSprop(policy_net.parameters(), lr=config["learning_rate"])
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
@@ -131,86 +150,79 @@ def epsilon_greedy(epsilon, state):
     
     # exploit
     else: 
-        # print("Exploit!")
-        q_values = policy_net.forward_pass(state)
+        # print("exploit!")
+        q_values = policy_net.forward_pass(state)[0]
+        max_q_value = max(q_values)
+        episode_q_values.append(max_q_value.detach().numpy())
         action = torch.argmax(q_values).item()
 
     return action
 
 
 if __name__=="__main__":
-    
-    # https://gym.openai.com/envs/#atari
-    games = ["BeamRider-v0", "Breakout-v0", "Enduro-v0", "Pong-v0", "Qbert-v0", "Seaquest-v0", "SpaceInvaders-v0"]
 
-    # for initial testing use cartpole https://gym.openai.com/envs/CartPole-v1/
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     config = {
-        "learning_rate": 0.001,
-        "minibatch_size": 128,
-        "num_episodes": 1_000,
-        "max_time": 10_000,
-        "discount": 0.99,
-        "epsilon": 1
-
+        "learning_rate": 3e-4,
+        "minibatch_size": 64,
+        "num_episodes": 3000,
+        "max_episode_time": 10_000,
+        "discount": 0.999,
+        "epsilon": 1,
+        "num_frame_stack": 4,
+        "memory_capacity": 10_000
     }
 
     if WANDB:
         wandb.config = config
 
-    episode_durations = []
-
-    num_frame_stack = 4
-    env = make_environment("CartPole-v1", num_frame_stack)
+    # https://gym.openai.com/envs/#atari
+    # for initial testing use cartpole https://gym.openai.com/envs/CartPole-v1/
+    games = ["BeamRider-v0", "Breakout-v0", "Enduro-v0", "Pong-v0", "Qbert-v0", "Seaquest-v0", "SpaceInvaders-v0"]
+    env = make_environment("CartPole-v1", config["num_frame_stack"])
     num_actions = env.action_space.n
 
     # inputs for taking actions
-    memory = MemoryBuffer(1_000_000)
-    num_episodes = 100
-    max_time = 10_000
-    discount = 0.99
+    memory = MemoryBuffer(config["memory_capacity"])
 
     # anneal epsilon linearly for 1 million
     # frames until 0.1, then keep fixed
     initial_epsilon = 1
     final_epsilon = 0.1
-    num_anneal_frames = 1_000_000 
+    num_anneal_frames = 10_000
     decrease_per_frame = (initial_epsilon - final_epsilon) / num_anneal_frames
     frame_num = 0
     epsilon = initial_epsilon
-
-    # training hyperparameters
-    minibatch_size = 8
-    learning_rate = 3e-4
     
     # initialise DQNs
     policy_net = DQN(num_actions)
     target_net = DQN(num_actions)
 
-    for episode in range(num_episodes):
+    for episode in range(config["num_episodes"]):
         observation = env.reset()
-        state = torch.from_numpy(observation.__array__()) / 255
-        state = state.permute((3,0,1,2)).unsqueeze(0)
+        episode_rewards = 0
+        episode_q_values = []
 
-        if WANDB:
-            wandb.log({"loss": loss})
-
-        for t in range(max_time):
+        for t in range(config["max_episode_time"]):
 
             screen = env.render()
-            transition = [state]
+
+            state = get_state(observation)
             action = epsilon_greedy(epsilon, state)
             observation, reward, done, info = env.step(action)
+            next_state = get_state(observation)
 
             # store transition minibatch
-            next_state = torch.from_numpy(observation.__array__()) / 255
-            next_state = next_state.permute((3,0,1,2)).unsqueeze(0)
-            transition.extend([action, reward, next_state, done])
+            # differs from the original paper due to the inclusion of "done"
+            transition = [state, action, reward, next_state, done]
+            # transition = [state, action, reward, next_state]
             memory.store_transition(transition)
+            episode_rewards += reward
 
-            if len(memory) >= 10 * minibatch_size:
-                minibatch = memory.random_sample(minibatch_size)
-                train_model(minibatch, discount)
+            if len(memory) >= config["memory_capacity"]:
+                minibatch = memory.random_sample(config["minibatch_size"])
+                train_model(minibatch, config["discount"])
 
             if done:
                 print("Episode {} finished after {} timesteps".format(episode+1, t+1))
@@ -220,5 +232,8 @@ if __name__=="__main__":
             frame_num += 1
             if frame_num <= num_anneal_frames:
                 epsilon -= decrease_per_frame
+
+        avg_reward = episode_rewards
+        avg_q_value = np.mean(episode_q_values)
 
     env.close()
